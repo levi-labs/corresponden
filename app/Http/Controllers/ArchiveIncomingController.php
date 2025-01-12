6ca4fee023fae2a4c5d27aa24bf5e51c10c5819a
rp.quraisy@gmail.com <rp.quraisy@gmail.com>
2025-01-12 11:32:09 +0700

    feat(archive-incoming): add archive controller incoming letter

---
+++ app/Http/Controllers/ArchiveIncomingController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ArchiveIncomingLetter;
+use Illuminate\Http\Request;
+
+class ArchiveIncomingController extends Controller
+{
+    public function index()
+    {
+        $title = 'Archive Incoming Letters';
+
+        return view('pages.message.incoming.archive', compact('title'));
+    }
+
+    public function show(ArchiveIncomingLetter $archiveIncomingLetter)
+    {
+        $title = 'Letter Details';
+        return view('pages.message.incoming.detail', compact('title'));
+    }
+    public function create()
+    {
+        $title = 'Create Incoming Letter';
+        return view('pages.message.incoming.create');
+    }
+
+    public function store()
+    {
+        $title = 'Create Incoming Letter';
+        return redirect()->back()->with('success', 'Archive Incoming Letter Created successfully');
+    }
+
+    public function edit()
+    {
+        $title = 'Edit Incoming Letter';
+        return view('pages.message.incoming.edit');
+    }
+    public function update()
+    {
+        $title = 'Edit Incoming Letter';
+        return redirect()->back()->with('success', 'Archive Incoming Letter Updated successfully');
+    }
+}
